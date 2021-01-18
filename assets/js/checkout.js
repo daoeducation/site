da71bccecb7db5f5bb59031c89556de2a4bb1fb2
nubis <yo@nubis.im>
2021-01-18 20:25:49 -0300

    Disables recaptcha for payments page

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -7,28 +7,13 @@ var initCheckout = function(api_url, program){
   };
 
   $(function(){
-    fetch(`${api_url}/public_config`)
-      .then(function (result) { return result.json(); })
-      .then(function (config) {
-        window.recaptcha_loaded = function(){
-          grecaptcha.ready(function() {
-            grecaptcha.execute(config.recaptcha_key, {action: 'submit'}).then(function(token) {
-              $.post(
-                `${api_url}/stripe/checkout_sessions`,
-                JSON.stringify({ recaptcha_token: token, program: program })
-              ).done(function(data){
-                Stripe(config.stripe_key).redirectToCheckout({sessionId: data.id }).then(handleResult);
-              });
-            });
-          });
-        }
-
-        $('<script>')
-          .attr('type', 'text/javascript')
-          .attr('async', '')
-          .attr('defer', '')
-          .attr('src', "https://www.google.com/recaptcha/api.js?onload=recaptcha_loaded&render=" + config.recaptcha_key)
-          .appendTo('body');
-      })
+    $.get(`${api_url}/public_config`, function(config){
+      $.post(
+        `${api_url}/stripe/checkout_sessions`,
+        JSON.stringify({ recaptcha_token: 'disabled', program: program })
+      ).done(function(data){
+        Stripe(config.stripe_key).redirectToCheckout({sessionId: data.id }).then(handleResult);
+      });
+    })
   });
 }
