66b1feddda97317044519a3c0ee4720c551044c2
bronxi <57157439+bronxi47@users.noreply.github.com>
2021-01-28 00:11:50 -0300

    Update sample_spec.rb

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,3 +1,25 @@
+describe "payment_pages", type: :feature, js: true do
+  it "can pay for the zero-to-hero program" do
+    visit '/payments/zero_to_hero.html'
+
+    expect(page).to have_selector ".ProductSummary-info", wait: 30
+    expect(page).to have_content "Zero to Hero"
+  end
+
+  it "can pay for the coding bootcamp program" do
+    visit '/payments/coding_bootcamp.html'
+
+    expect(page).to have_selector ".ProductSummary-info", wait: 30
+    expect(page).to have_content "Coding Bootcamp"
+  end
+
+  it "can pay with BTC after signup success" do
+    visit '/signup_success.html'
+
+    find("#btc_button").click
+    expect(page).to have_content "Un servicio de Bitex"
+
+  end
 
   it "can pay with paypal after signup success" do
     visit '/signup_success.html'
@@ -5,3 +27,8 @@
     find("#paypal_button").click
     expect(page).to have_content "Pagar con PayPal"
   end
+
+  end
+
+
+end
