d517048780825eb6907d95fe9a9c5faaab1882b4
bronxi <57157439+bronxi47@users.noreply.github.com>
2021-01-28 11:07:32 -0300

    Update sample_spec.rb

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,17 +1,6 @@
 describe "payment_pages", type: :feature, js: true do
   it "can pay for the zero-to-hero program" do
-    visit '/payments/zero_to_hero.html'
 
-    expect(page).to have_selector ".ProductSummary-info", wait: 30
-    expect(page).to have_content "Zero to Hero"
-  end
-
-  it "can pay for the coding bootcamp program" do
-    visit '/payments/coding_bootcamp.html'
-
-    expect(page).to have_selector ".ProductSummary-info", wait: 30
-    expect(page).to have_content "Coding Bootcamp"
-  end
 
   it "can pay with BTC after signup success" do
     visit '/signup_success.html'
