8c7e10b1bdc618cd2c91885b5a943f731179153b
bronxi <57157439+bronxi47@users.noreply.github.com>
2021-01-29 10:56:32 -0300

    Update sample_spec.rb

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,17 +1,11 @@
-describe "payment_pages", type: :feature, js: true do
+describe "visit_test", type: :feature, js: true do
   
   it "can pay with BTC after signup success" do
     visit '/signup_success.html'
-
-    find("#btc_button").click
- 
-
   end
 
-  it "can pay with paypal after signup success" do
-    visit '/signup_success.html'
-
-    find("#paypal_button").click
-   
+  it "can visit programas page" do
+    visit '/programas.html'
   end
+  
 end
