c1a6a743289c3959c59cf6dd8d838c1321eea49e
bronxi <57157439+bronxi47@users.noreply.github.com>
2021-02-02 19:58:16 -0300

    Update sample_spec.rb

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,11 +1,17 @@
 describe "visit_test", type: :feature, js: true do
+  it "can pay with BTC after signup success" do
+  visit '/about_us.html'
+  end
+  it "can pay with BTC after signup success" do
+  visit '/profesores.html'
+  end  
+  
+    it "can visit programas page" do
+    visit '/programas.html'
+  end
   
   it "can pay with BTC after signup success" do
     visit '/signup_success.html'
-  end
-
-  it "can visit programas page" do
-    visit '/programas.html'
   end
   
 end
